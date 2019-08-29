c5f40a12bd4d3e0c9292ca071512fceb9ee0f570
Andrew <nk-andrew1987@mail.ru>
2019-08-29 12:40:24 +0300

    Выполнен слайдер

--- js/script.js
+++ js/script.js
@@ -170,13 +170,68 @@ window.addEventListener('DOMContentLoaded', function () {
 	  };
    };
    
-   postData(formData)
-                     .then(() => statusMessage.innerHTML = message.loading)
-                     .then(() => statusMessage.innerHTML = message.success)
-                     .catch(() => statusMessage.innerHTML = message.failure)
-                     .then(clearInput);
- };
+    postData(formData)
+                      .then(() => statusMessage.innerHTML = message.loading)
+                      .then(() => statusMessage.innerHTML = message.success)
+                      .catch(() => statusMessage.innerHTML = message.failure)
+                      .then(clearInput);
+  };
 
- mainForm.addEventListener('submit', createRequestForm);
+  mainForm.addEventListener('submit', createRequestForm);
+
+
+  // Слайдер
+
+  let slides = document.querySelectorAll('.slider-item');
+  let prev = document.querySelector('.arrow-left');
+  let next = document.querySelector('.arrow-right');
+  let dotsWrap = document.querySelector('.slider-dots');
+  let dots = document.querySelectorAll('.dot');
+
+  let startIndex = 1;
+
+  function showSlide (n) {
+    if (n > slides.length) {
+      startIndex = 1;
+    }
+
+    if (n < 1) {
+      startIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+
+    dots.forEach((item) => item.classList.remove('dot-active'));
+
+    slides[startIndex - 1].style.display = 'block';
+    dots[startIndex - 1].classList.add('dot-active');
+
+  };
+
+  showSlide(startIndex);
+
+  function plusSlide (n) {
+    showSlide(startIndex += n);
+  }
+
+  function currentSlide (n) {
+    showSlide(startIndex = n);
+  }
+
+  prev.addEventListener('click', function () {
+    plusSlide(-1);
+  });
+
+  next.addEventListener('click', function () {
+    plusSlide(1);
+  });
+
+  dotsWrap.addEventListener('click', function (evt) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (evt.target.classList.contains('dot') && event.target == dots[i-1]) {
+        currentSlide(i);
+      };
+    };
+  });
 
 })
